8f963cd89b7e9eddc6a70e8b3b43d8fe93252653
Elma-dev <67378945+Elma-dev@users.noreply.github.com>
2023-09-30 20:05:36 +0100

    supplier commit

---
+++ kafka_supplaier_producer/src/main/java/dev/elma/kafka_supplaier_producer/KafkaSupplaierProducerApplication.java
@@ -0,0 +1,13 @@
+package dev.elma.kafka_supplaier_producer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaSupplaierProducerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KafkaSupplaierProducerApplication.class, args);
+    }
+
+}
---
+++ kafka_supplaier_producer/src/test/java/dev/elma/kafka_supplaier_producer/KafkaSupplaierProducerApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.elma.kafka_supplaier_producer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaSupplaierProducerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
