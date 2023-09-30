6fb0b5d2de6546b523b22b4f58bdc75a6c871ae3
Elma-dev <67378945+Elma-dev@users.noreply.github.com>
2023-09-30 21:33:22 +0100

    add func prod cons

---
+++ kafka_function_cons_prod/src/main/java/dev/elma/kafka_function_cons_prod/KafkaFunctionConsProdApplication.java
@@ -0,0 +1,13 @@
+package dev.elma.kafka_function_cons_prod;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaFunctionConsProdApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KafkaFunctionConsProdApplication.class, args);
+    }
+
+}
---
+++ kafka_function_cons_prod/src/test/java/dev/elma/kafka_function_cons_prod/KafkaFunctionConsProdApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.elma.kafka_function_cons_prod;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaFunctionConsProdApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
